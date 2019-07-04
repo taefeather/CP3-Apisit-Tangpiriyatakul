ca9d32e91d646070c56a5bd533b0ca820ecf8b77
taefeather <49952502+taefeather@users.noreply.github.com>
2019-07-04 21:07:03 +0700

    Add files via upload

---
+++ Exercise4_Apisit_T..py
@@ -0,0 +1,10 @@
+F = 80
+G = 75
+I = 90
+C = 85
+
+print("----- Your Score -----")
+print("Foundation English :",F ,"Point")
+print("General Business :",G ,"Point")
+print("Introduction to Computer Systems :",I ,"Point")
+print("Computer Programming :",C ,"Point")
